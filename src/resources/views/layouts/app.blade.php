7c0ba9ef0c4f0b517772ce9aefb2f25dc00c5947
ikeda rion <ike0324rion@icloud.com>
2025-01-12 18:34:14 +0900

    Update layout CSS and view

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FreeMarket</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sanitize.css/13.0.0/sanitize.min.css">
+    <link rel="stylesheet" href="{{ asset('css/reset.css')}}">
     <link rel="stylesheet" href="{{ asset('css/common.css')}}">
     @yield('css')
 </head>
