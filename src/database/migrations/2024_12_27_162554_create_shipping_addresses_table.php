b7d8853f59a1e9dbec3f82436595692c74bdd0bf
ikeda rion <ike0324rion@icloud.com>
2025-01-14 11:40:02 +0900

    Modify shipping_addresses table migration

--- src/database/migrations/2024_12_27_162554_create_shipping_addresses_table.php
+++ src/database/migrations/2024_12_27_162554_create_shipping_addresses_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('postal_code','20');
             $table->string('address');
-            $table->string('building_name')->nullable();
+            $table->string('building_name');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
